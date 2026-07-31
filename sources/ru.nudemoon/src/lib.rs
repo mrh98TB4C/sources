192a61bc26ad49524a2f580e14df85551e5ede5d
Aleksandr Beshkenadze <beshkenadze@gmail.com>
2026-07-31 23:10:24 +0300

    chore(ru.nudemoon): remove all diagnostic logging, bump to v21

--- sources/ru.nudemoon/src/lib.rs
+++ sources/ru.nudemoon/src/lib.rs
@@ -452,11 +452,6 @@ impl WebLoginHandler for Nudemoon {
 		if key != "login" {
 			bail!("Invalid login key: `{key}`");
 		}
-		println!(
-			"NudeMoon WebLogin: received {} cookies, keys={:?}",
-			cookies.len(),
-			cookies.keys().collect::<Vec<_>>()
-		);
 		Ok(auth::save_cookies(&cookies))
 	}
 }
